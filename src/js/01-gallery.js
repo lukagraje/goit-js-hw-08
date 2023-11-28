8df895064689bf72d5428256506c1947a8be9235
lukagraje <138776692+lukagraje@users.noreply.github.com>
2023-11-28 19:27:17 +0100

    homework done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
+
+const galleryContainer = document.querySelector(`ul.gallery`);
+
+const images = galleryItems.map((item) => {
+    return `<li>
+                <a class="gallery__item" href="${item.original}">
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>
+            </li>`;    
+});
+galleryContainer.insertAdjacentHTML('beforeend', images.join(""));
+
+const galleryImage = document.querySelectorAll(".gallery__item");
+
+
+new SimpleLightbox(galleryImage, {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+})
+
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const email = document.querySelector("input[name = email]");
+const message = document.querySelector("textarea[name = message]");
+const form = document.querySelector(".feedback-form");
+const feedback = { email, message };
+
+function formSaver() {
+    feedback.email = email.value;
+    feedback.message = message.value;    
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedback));
+}
+
+form.addEventListener('input', throttle(formSaver, 500));
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();   
+    console.log(feedback);
+    localStorage.removeItem('feedback-form-state');
+    email.value = null;
+    message.value = null;
+    feedback.email = null;
+    feedback.message = null;
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    try {
+        email.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+        message.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
+    } catch (error) {
+        console.log(error.name);
+        console.log(error.value);
+    }    
+})
